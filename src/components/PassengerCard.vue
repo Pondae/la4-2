6971a155e7648fd592b676f697d8dc73d07b96b2
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-24 17:13:10 +0700

    chk pt1: update vue and fetch passengers data

---
+++ src/components/PassengerCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="passenger-card">
+      <span>@{{ passenger.name }} ,Id: {{ passenger._id }}</span>
+      <h4>{{ passenger.trips }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PassengerCard',
+  props: {
+    passenger: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.passenger-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.passenger-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Total Passengers</h1>
+  <div class="passengers">
+    <PassengerCard v-for="passenger in passengers" :key="passenger._id" :passenger="passenger" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import PassengerCard from '@/components/PassengerCard.vue'
+import PassengerService from '@/services/PassengerService.js'
+import { watchEffect } from '@vue/runtime-core'
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    PassengerCard
   },
-};
+  data() {
+    return {
+      passengers: null,
+    }
+  },
+  created() {
+    watchEffect(() => {
+    PassengerService.getPassengers(0,10)
+    .then((response) => {
+      this.passengers = response.data.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    })
+  }
+}
 </script>
+<style scoped>
+.passengers {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
